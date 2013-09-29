bbd1c37ea99a501f68a004f187f39ba782332141
Elie Roux <elie.roux@telecom-bretagne.eu>
2013-09-29 16:35:33 +0200

    Initial commit, empty parser

---
+++ syntax.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Dpokuwiki CMK plugin
+ * Copyright (C) 2013 Elie Roux <elie.roux@telecom-bretagne.eu>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * --------------------------------------------------------------------
+ */
+
+if (!defined('DOKU_INC')) die();
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_cmk extends DokuWiki_Syntax_Plugin {
+    /**
+     * @return string Syntax mode type
+     */
+    public function getType() {
+        return 'substition';
+    }
+    /**
+     * @return string Paragraph type
+     */
+    public function getPType() {
+        return 'normal';
+    }
+    /**
+     * @return int Sort order - Low numbers go before high numbers
+     *
+     * CMK Markup rendering should be able to use other patters so this is
+     * low.
+     */
+    public function getSort() {
+        return 55;
+    }
+
+    /**
+     * Connect lookup pattern to lexer.
+     *
+     * @param string $mode Parser mode
+     */
+    public function connectTo($mode) {
+        $this->Lexer->addEntryPattern('<FIXME>',$mode,'plugin_cmk');
+    }
+
+    public function postConnect() {
+        $this->Lexer->addExitPattern('</FIXME>','plugin_cmk_cmk');
+    }
+
+    /**
+     * Handle matches of the cmk syntax
+     *
+     * @param string $match The match of the syntax
+     * @param int    $state The state of the handler
+     * @param int    $pos The position in the document
+     * @param Doku_Handler    $handler The handler
+     * @return array Data for the renderer
+     */
+    public function handle($match, $state, $pos, &$handler){
+        $data = array();
+
+        return $data;
+    }
+
+    /**
+     * Render xhtml output or metadata
+     *
+     * @param string         $mode      Renderer mode (supported modes: xhtml)
+     * @param Doku_Renderer  $renderer  The renderer
+     * @param array          $data      The data from the handler() function
+     * @return bool If rendering was successful.
+     */
+    public function render($mode, &$renderer, $data) {
+        if($mode != 'xhtml') return false;
+
+        return true;
+    }
+}
+}
+?>
